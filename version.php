e6347df3fb4c01511ee6d07e3a074476c0c8f098
Michael Ruiz <mikeaveli007@hotmail.com>
2021-10-31 19:36:55 -0700

    update version info

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015073104;
+$plugin->version   = 2019111900;
 $plugin->component = 'block_my_enrolled_courses';
 $plugin->requires  = 2013051400;
 $plugin->release = '2.4.1 (Build: 2015073100)';
